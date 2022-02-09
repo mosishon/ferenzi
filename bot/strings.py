bcf4b0841d7128c0a9b98ad650607cc41061fd89
Mostafa Arshadi <mostafa1382ma@gmail.com>
2022-02-10 02:23:32 +0330

    Add New bot text

--- bot/strings.py
+++ bot/strings.py
@@ -1,5 +1,20 @@
 from bot.constants import (SUPER_SUDO_USERNAME)
 
+# start texts
 START_ADMIN = "سلام ادمین عزیز خوش آمدید"
 START_USER = (" سلام کاربر عزیز. این ربات صراف جهت مدیریت گروه طب سنتی میباشد"
             "برای دریافت اطلاعات بیشتر با سودو در ارتباط باشید  ")
+START_ADMIN_GROUP = "این دستور مختص سودوی ربات میباشد"
+START_SUDO_GROUP  = "دستور از سمت سودوی ربات دریافت شد.\n ربات بزودی در گروه شما فعال خواهد شد"
+# install/uninstall texts
+BOT_ALREADY_INSTALLED = ("ربات در گروه شما فعال شده است\n"
+                        "نیاز به نصب مجدد ربات نیست")
+BOT_INSTALLED_SUCCESSFULLY = "ربات با موفقیت نصب شد"
+BOT_UNINSTALLED_SUCCESSFULLY = "ربات با موفقیت حذف شد"
+BOT_ISNOT_INSTALLED = "ربات در گروه شما فعال نیست"
+I_LEAVE_GROUP = "این گروه از لیست گروه های نصب شده خارج شد.\nربات ها به زودی از گروه خارج میشوند"
+MAKE_ME_ADMIN = "لطفا ابتدا برای اجرای تمامی دستورات  من را به ادمین گروه ارتقا دهید"
+IM_HERE_TO_HELP = "سلام.\nمن ربات دو قلو و کمکی فرنزی هستم." #TODO: bot name should be dynamic
+# errors texts
+UNKNOWN_ERROR_OCURRED = "خطای ناشناخته رخ داده است"
+THIS_COMMAND_IS_NOT_AVAILABLE_FOR_YOU = "این دستور برای شما در دسترس نمیباشد"
