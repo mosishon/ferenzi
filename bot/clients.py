2902175b3c56de23d71caf86a8797679484b36a7
Mostafa Arshadi <mostafa1382ma@gmail.com>
2022-02-10 02:22:04 +0330

    move clients to bot package

---
+++ bot/clients.py
@@ -0,0 +1,10 @@
+from bot.constants import (SESSION_NAME,SESSION2_NAME,API_ID,API_HASH)
+from bot.config import PROXY
+from telethon import TelegramClient
+
+
+# Create a TelegramClient instance (API Bot)
+client = TelegramClient(SESSION_NAME, API_ID, API_HASH,proxy=PROXY)
+
+# Create a client instance (User)
+client_user = TelegramClient(SESSION2_NAME, API_ID, API_HASH,proxy=PROXY)
