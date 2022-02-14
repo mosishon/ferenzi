e8f0f1369f6e8da597eaa8b6203a59fc3bf7d6da
Mostafa Arshadi <mostafa1382ma@gmail.com>
2022-02-14 15:00:46 +0330

    add configure,cli name, cli name, panel, clear text

--- bot/strings.py
+++ bot/strings.py
@@ -6,6 +6,7 @@ START_USER = (" سلام کاربر عزیز. این ربات صراف جهت م
             "برای دریافت اطلاعات بیشتر با سودو در ارتباط باشید  ")
 START_ADMIN_GROUP = "این دستور مختص سودوی ربات میباشد"
 START_SUDO_GROUP  = "دستور از سمت سودوی ربات دریافت شد.\n ربات بزودی در گروه شما فعال خواهد شد"
+
 # install/uninstall texts
 BOT_ALREADY_INSTALLED = ("ربات در گروه شما فعال شده است\n"
                         "نیاز به نصب مجدد ربات نیست")
@@ -15,6 +16,24 @@ BOT_ISNOT_INSTALLED = "ربات در گروه شما فعال نیست"
 I_LEAVE_GROUP = "این گروه از لیست گروه های نصب شده خارج شد.\nربات ها به زودی از گروه خارج میشوند"
 MAKE_ME_ADMIN = "لطفا ابتدا برای اجرای تمامی دستورات  من را به ادمین گروه (تیک افزودن ادمین نیاز میباشد) ارتقا دهید"
 IM_HERE_TO_HELP = "سلام.\nمن ربات دو قلو و کمکی فرنزی هستم." #TODO: bot name should be dynamic
+
+# configure group texts
+CONFIGURE_ADMINS = "آیا مایل به پیکربندی ربات و تنظیمات آن هستید؟\n ادمین ها شناسایی و به ادمین های ربات افزوده میشوند"
+ADMINS_CONFIGURED = ("تعداد {} ادمین شناسایی و با موفقیت به ادمین های ربات افزوده شد") # param1: number of admins
 # errors texts
 UNKNOWN_ERROR_OCURRED = "خطای ناشناخته رخ داده است"
 THIS_COMMAND_IS_NOT_AVAILABLE_FOR_YOU = "این دستور برای شما در دسترس نمیباشد"
+
+# names texts
+BOT_CLI_TITLE = "Ferenzi Cli"
+
+#panel
+    #texts
+PANEL_TEXT = "پنل مدیریتی"
+    # options
+PANEL_TEXTS_TO_OPEN = ("/panel","پنل","panel")
+
+#clear
+CLEAR_ALL = ("پاکسازی کلی","پاکسازی کل")
+CLEARING_STARTED = "فرآیند پاکسازی پیام ها شروع شد"
+CLEATING_FINISHED = "پاکسازی پیام ها به پایان رسید"
