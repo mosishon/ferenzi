cf81bb755bca4eb10c51c4d3479ee56c4a51724c
Mostafa Arshadi <mostafa1382ma@gmail.com>
2022-03-19 07:36:35 +0330

    add answer to a word for admin and user & add word/answer command & add DEMOTE and PROMOTE admin command

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,13 +2,13 @@ import asyncio
 import logging
 import telethon
 from telethon.tl.custom import Message
-from bot.constants import BOT_MESSAGE_DELTE_TIME, MINIMUM_CHAR_LIMIT, SUPER_SUDO_ID
+from bot.constants import ANSWER_TYPES, BOT_MESSAGE_DELTE_TIME, MINIMUM_CHAR_LIMIT, SUPER_SUDO_ID
 
 from bot.db import C_GROUPS
 from bot.exceptions import GroupAlreadyExists, GroupNotExists
-from bot.strings import (BOT_ALREADY_INSTALLED,ADMINS_CONFIGURED, BOT_INSTALLED_SUCCESSFULLY, BOT_ISNOT_INSTALLED, BOT_UNINSTALLED_SUCCESSFULLY, CHAT_LIMIT_PANEL, CLEAR_ALL, CLEATING_FINISHED, ENTER_CHAR_LIMIT, I_LEAVE_GROUP, IM_HERE_TO_HELP, MAKE_ME_ADMIN, PANEL_TEXT, PANEL_TEXTS_TO_OPEN, SHOULD_BE_GREATER_THAN_MINIMUM_CHAR_LIMIT, SHOULD_BE_NUMBER, START_ADMIN, START_SUDO_GROUP,START_USER, SUCCESFUL_CHAR_LIMIT_SET, THIS_COMMAND_IS_NOT_AVAILABLE_FOR_YOU, UNKNOWN_ERROR_OCURRED,BOT_CLI_TITLE,CONFIGURE_ADMINS)
+from bot.strings import (ADD_ANSWER, ADMIN_DEMOTE_SUCCESSFULLY, ADMIN_PROMOTE_SUCCESSFULLY, ANSEWR_TYPE_INVALID, ANSWER_ADDED, BOT_ALREADY_INSTALLED,ADMINS_CONFIGURED, BOT_INSTALLED_SUCCESSFULLY, BOT_ISNOT_INSTALLED, BOT_UNINSTALLED_SUCCESSFULLY, CHAT_LIMIT_PANEL, CLEAR_ALL, CLEATING_FINISHED, DEMOTE, ENTER_CHAR_LIMIT, I_LEAVE_GROUP, IM_HERE_TO_HELP, MAKE_ME_ADMIN, PANEL_TEXT, PANEL_TEXTS_TO_OPEN, PROMOTE, SHOULD_BE_GREATER_THAN_MINIMUM_CHAR_LIMIT, SHOULD_BE_NUMBER, SHOULD_REPLIED, START_ADMIN, START_SUDO_GROUP,START_USER, SUCCESFUL_CHAR_LIMIT_SET, THIS_COMMAND_IS_NOT_AVAILABLE_FOR_YOU, UNKNOWN_ERROR_OCURRED,BOT_CLI_TITLE,CONFIGURE_ADMINS)
 from bot.buttons import (BACK_TO_CHAT_LIMIT_PANEL, CONTACT_SUPER_ADMIN,CONFIGURE_ADMINS_BTN, PANEL_CHAR_LIMIT_BTN, PANEL_HOME_BTN, PANEL_LOCKS_BTN)
-from bot.utils import add_new_admin, add_new_group, add_new_user, call_async, check_admin_access, check_group, clear_all_message, configure_group_admins, delete_after, delete_group, get_media_type, get_status, is_owner, is_sudo, join_group, leave_group, process_char_limit_delete, process_media_delete, process_profile_photo_delete, set_char_limit, set_status, toggle_char_limit, update_lock
+from bot.utils import add_answer, add_new_admin, add_new_group, add_new_user, call_async, check_admin_access, check_answer, check_group, clear_all_message, configure_group_admins, delete_admin, delete_after, delete_group, get_media_type, get_status, is_owner, is_sudo, join_group, leave_group, process_char_limit_delete, process_media_delete, process_profile_photo_delete, set_char_limit, set_status, toggle_char_limit, update_lock
 from bot.clients import client_user,client
 
 async def handle_message(message:Message):
@@ -66,8 +66,12 @@ async def handle_group_message_admin(message:Message):
     if "=>" in status:
         args = status.split("=>")[1].split("-")
         status = status.split("=>")[0]
-    print(args,status)
     logging.debug(f"Received message: {text} from chat {chat_id} from user {user_id} on handle_group_message_admin")
+    if not is_sudo(user_id):
+        answer_ = check_answer(chat_id,message,0)
+        if answer_:
+            await message.reply(answer_,link_preview=False)
+            
     if text.lower() in ("/start","نصب"):
         # Check if the user is a sudo or admin
 
@@ -147,10 +151,40 @@ async def handle_group_message_admin(message:Message):
         else:
             await message.respond(SUCCESFUL_CHAR_LIMIT_SET,buttons=PANEL_CHAR_LIMIT_BTN(chat_id,user_id))
             set_char_limit(chat_id,int(text))
-    if is_owner(user_id):
+    elif text.startswith(ADD_ANSWER):
+        text = text.replace(ADD_ANSWER,"")
+        reply = await message.get_reply_message()
+        if not reply:
+            return await message.respond(SHOULD_REPLIED)
+        answer = reply.text
+    
+        answer_type = text.split(" ")[-1]
+        word =  text.replace(answer_type,"").strip()
+        ANSWER_TYPE = ANSWER_TYPES.get(answer_type.strip().lower())
+        if ANSWER_TYPE is None:
+            return await message.respond(ANSEWR_TYPE_INVALID)
+        add_answer(chat_id,word,answer,ANSWER_TYPE)
+        return await message.respond(ANSWER_ADDED.format(word,answer,answer_type))
+
+     
+    if await is_owner(user_id,chat_id,client):
         if text.lower() in CONFIGURE_ADMINS:
             count = await configure_group_admins(chat_id,client)
             await message.respond(ADMINS_CONFIGURED.format(count))
+        elif text.lower() in DEMOTE:
+            reply = await message.get_reply_message()
+            if not reply:
+                return await message.respond(SHOULD_REPLIED)
+            repied_id = reply.sender_id
+            delete_admin(chat_id,repied_id)
+            await message.respond(ADMIN_DEMOTE_SUCCESSFULLY.format(repied_id))
+        elif text.lower() in PROMOTE:
+            reply = await message.get_reply_message()
+            if not reply:
+                return await message.respond(SHOULD_REPLIED)
+            repied_id = reply.sender_id
+            add_new_admin(chat_id,repied_id)
+            await message.respond(ADMIN_PROMOTE_SUCCESSFULLY.format(repied_id))
     
 async def handle_group_callback_admin(message:telethon.events.CallbackQuery.Event):
     """
@@ -223,13 +257,13 @@ async def handle_group_message_user(message:Message):
     chat_id = message.chat_id
     if not check_group(chat_id):
         return
+    
     user_id = message.sender_id
     chat = await message.get_input_chat()
     sender = await message.get_input_sender()
     text = message.text
     media = message.media
     add_new_user(user_id,chat_id)
-    print(message.via_bot)
     logging.debug(f"Received message: {text} from chat {chat_id} from user {user_id} on handle_group_message_user")
     if await process_media_delete(media,chat_id,message):
         return
@@ -237,3 +271,6 @@ async def handle_group_message_user(message:Message):
         return
     elif await process_profile_photo_delete(chat_id,user_id,message,client):
         return
+    answer_ = check_answer(chat_id,message,1)
+    if answer_:
+        await message.reply(answer_,link_preview=False)
